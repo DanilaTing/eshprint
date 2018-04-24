8461d170f960d9a25cc8feaf3ced3f7e1d7b9b86
DanilaTing <tinggone@gmail.com>
2018-04-25 00:58:47 +0300

    yo

--- LASTSEEN/javascripts/timer.js
+++ LASTSEEN/javascripts/timer.js
@@ -17,8 +17,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("timer").innerHTML = days + ":" + hours + ":"
-  + minutes + ":" + seconds + "";
+  document.getElementById("timer").innerHTML = days + " : " + hours + " : "
+  + minutes + " : " + seconds + "";
 
   // If the count down is finished, write some text
   if (distance < 0) {
