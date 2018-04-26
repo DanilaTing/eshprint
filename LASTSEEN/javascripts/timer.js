1f8150c980f53e2f317faa9c02e3ab7d880a9018
DanilaTing <tinggone@gmail.com>
2018-04-26 21:27:03 +0300

    123

--- LASTSEEN/javascripts/timer.js
+++ LASTSEEN/javascripts/timer.js
@@ -9,12 +9,31 @@ var x = setInterval(function() {
 
   // Find the distance between now an the count down date
   var distance = countDownDate - now;
+  console.log("distance = " + distance);
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  console.log(days);
+
+  if (days < 10) {
+    days = "0" + days.toString()
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours.toString()
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes.toString()
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds.toString()
+  }
 
   // Display the result in the element with id="demo"
   document.getElementById("timer").innerHTML = days + " : " + hours + " : "
